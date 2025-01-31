125da11aa3224ef7ddbf61ad5d6d4b51facb18b4
Laura Tchartorijsky <lauratcharto@gmail.com>
2025-01-31 15:57:11 +0100

    fix css parsing problem by jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
     testEnvironment: "jsdom",
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    }
   };
   
---
+++ src/__tests__/searchBar.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import SearchBar from '../components/SearchBar/SearchBar';
+
+describe('SearchBar', () => {
+    /*test("searchbar input renders", () => {
+        render(<SearchBar />); //render searchbar
+        const inputName = screen.getByRole('input', { name: /searchTerm/i }); // get input value (placeholder)
+        expect(inputName).toBeInTheDocument(); //verifies that the input is in the document
+    });*/
+    
+    /*test("searchbar search button renders", () => {
+        render(<SearchBar/>); //renders searchBar
+    });*/
+});
