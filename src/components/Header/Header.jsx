25a7b245d137bc8f675b7403a730bc4ba3006ba9
Laura Tchartorijsky <lauratcharto@gmail.com>
2025-01-31 14:52:30 +0100

    basic header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import './header.css';
 export default function Header() {
     return (
         <header>
+            <img src="/reddit_orange.png" alt='orange reddit logo' width='100px' height='100px' />
             <h1>My Reddit Reader</h1>
             <SearchBar/>
         </header>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export default function SearchBar() {
-    return <>
-        <input type="search" id="searchTerm" name="searchTerm" maxlength="30" placeholder="I want to search for..."/>
-        <button name="search">Search</button>
-    </>;
+    return (
+        <div id='searchBar'>
+            <input type="search" id="searchTerm" name="searchTerm" maxLength="30" placeholder="I want to search for..."/>
+            <button name="search">Search</button>
+        </div>
+    )
 };
